23d8ecf3af7b6b2b01f7debd5bcf90973dca9bda
Nikita Chupahin <nik123chupahin@gmail.com>
2025-10-28 13:53:25 +0200

    added tests

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,13 +8,63 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('test@mail.com')).toBe('boolean');
+    expect(typeof validateEmail('false@email')).toBe('boolean');
   });
 
   it(`should return 'true' for the valid email`, () => {
-    expect(validateEmail('test838@gmail.com.'))
+    expect(validateEmail('test@mail.com'))
       .toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'true' for the email with digits`, () => {
+    expect(validateEmail('test838@gmail.com'))
+      .toBeTruthy();
+  });
+
+  it(`should return 'true' for the email with characters - and _`, () => {
+    expect(validateEmail('test_83-8@gmail.com'))
+      .toBeTruthy();
+  });
+
+  it(`should return 'true' for the email with min allowed symbols`, () => {
+    expect(validateEmail('t@q.c'))
+      .toBeTruthy();
+  });
+
+  it(`should return 'false' when email contains not English letters`, () => {
+    expect(validateEmail('тест@gmail.com'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' when email contains
+    double dots in personal_info part`, () => {
+    expect(validateEmail('test..@mail.com'))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' when email contains not allowed characters`, () => {
+    expect(validateEmail(`te!s$d%f&a'*+/=?^{|}~@mail.com`))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' when email wihout @`, () => {
+    expect(validateEmail(`testmail.com`))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' when personal_info part starts with .`, () => {
+    expect(validateEmail(`.test@mail.com`))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' when domain part starts with .`, () => {
+    expect(validateEmail(`test@.mail.com`))
+      .toBeFalsy();
+  });
+
+  it(`should return 'false' when email fails multiple rules`, () => {
+    expect(validateEmail(`false@email`))
+      .toBeFalsy();
+  });
 });
